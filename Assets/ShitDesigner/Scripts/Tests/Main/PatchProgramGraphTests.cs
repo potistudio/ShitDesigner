9a0f3e007ed173b9abf8ad54c704f8d176ba7217
potistudio <potistudio@proton.me>
2026-08-29 21:01:59 +0900

    Allow patches without 3D scene nodes

---
+++ Assets/ShitDesigner/Scripts/Tests/Main/PatchProgramGraphTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ShitDesigner.Main.Tests {
+	[TestFixture]
+	public sealed class PatchProgramGraphTests {
+		[Test]
+		public void Validate_AllowsGraphWithoutA3DSceneNode() {
+			var graph = new PatchProgramGraph(
+				"generator",
+				new[] { new PatchGraphNode("generator", "shitdesigner.shader.generator") },
+				new PatchGraphConnection[0]);
+
+			var result = graph.Validate();
+
+			Assert.That(result.IsSuccess, Is.True);
+		}
+	}
+}
