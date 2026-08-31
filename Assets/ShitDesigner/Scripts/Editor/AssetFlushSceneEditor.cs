46003f57a0e0f50ad5eb7718111cd2d3b8dfe9b4
POTI <potistudio@proton.me>
2026-09-01 04:39:34 +0900

    Add explicit AssetFlush size

---
+++ Assets/ShitDesigner/Scripts/Editor/AssetFlushSceneEditor.cs
@@ -0,0 +1,22 @@
+using ShitDesigner.AssetFlush;
+using UnityEditor;
+
+namespace ShitDesigner.Editor {
+	[CustomEditor(typeof(AssetFlushScene))]
+	public sealed class AssetFlushSceneEditor : UnityEditor.Editor {
+		private SerializedProperty m_FullScreen;
+		private SerializedProperty m_Size;
+
+		private void OnEnable() {
+			m_FullScreen = serializedObject.FindProperty("m_FullScreen");
+			m_Size = serializedObject.FindProperty("m_Size");
+		}
+
+		public override void OnInspectorGUI() {
+			serializedObject.Update();
+			DrawPropertiesExcluding(serializedObject, "m_Size");
+			if (!m_FullScreen.boolValue) EditorGUILayout.PropertyField(m_Size);
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
