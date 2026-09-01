381332f35e6ac5a77cf78cfb02863cbc16964061
POTI <potistudio@proton.me>
2026-09-01 12:53:11 +0900

    fix(scene): limit Chitose candy regeneration

--- Assets/ShitDesigner/Scripts/Tests/Scene/ChitoseCandyCutSceneTests.cs
+++ Assets/ShitDesigner/Scripts/Tests/Scene/ChitoseCandyCutSceneTests.cs
@@ -1,5 +1,7 @@
+using System.Reflection;
 using NUnit.Framework;
 using ShitDesigner.Core;
+using ShitDesigner.Main;
 using ShitDesigner.Scene;
 using UnityEngine;
 
@@ -52,6 +54,76 @@ namespace ShitDesigner.Tests.Scene {
 
 				cutScene.SetBpmClock(new BeatClockFrame(120f, 1d));
 
+				Assert.That(root.transform.GetChild(root.transform.childCount - 1), Is.SameAs(generatedRoot));
+			}
+			finally {
+				Object.DestroyImmediate(root);
+			}
+		}
+
+		[Test]
+		public void ChangingCutParametersDoesNotRebuildGeneratedCandy() {
+			var root = new GameObject("Chitose Candy Test");
+			try {
+				var cutScene = root.AddComponent<ChitoseCandyCutScene>();
+				var generatedRoot = root.transform.Find("Generated Chitose Candy");
+				Assert.That(generatedRoot, Is.Not.Null);
+
+				cutScene.SetSplitGap(1.25f);
+				cutScene.SetHorizontalImpulse(4.5f);
+				cutScene.SendMessage("OnValidate");
+				cutScene.SendMessage("Update");
+
+				Assert.That(root.transform.GetChild(root.transform.childCount - 1), Is.SameAs(generatedRoot));
+			}
+			finally {
+				Object.DestroyImmediate(root);
+			}
+		}
+
+		[Test]
+		public void ChangingCandyFieldParametersRebuildsGeneratedCandy() {
+			var root = new GameObject("Chitose Candy Test");
+			try {
+				var cutScene = root.AddComponent<ChitoseCandyCutScene>();
+				var generatedRoot = root.transform.Find("Generated Chitose Candy");
+				var candyCount = typeof(ChitoseCandyCutScene).GetField(
+					"m_CandyCount", BindingFlags.Instance | BindingFlags.NonPublic);
+				Assert.That(generatedRoot, Is.Not.Null);
+				Assert.That(candyCount, Is.Not.Null);
+
+				candyCount.SetValue(cutScene, 13);
+				cutScene.SendMessage("OnValidate");
+				cutScene.SendMessage("Update");
+
+				Assert.That(root.transform.GetChild(root.transform.childCount - 1), Is.Not.SameAs(generatedRoot));
+			}
+			finally {
+				Object.DestroyImmediate(root);
+			}
+		}
+
+		[Test]
+		public void LiveParametersUpdateCutWithoutRebuildingGeneratedCandy() {
+			var root = new GameObject("Chitose Candy Test");
+			try {
+				var cutScene = root.AddComponent<ChitoseCandyCutScene>();
+				var liveRoot = root.AddComponent<LiveSceneRoot>();
+				liveRoot.Initialize("chitose-candy-test");
+				var generatedRoot = root.transform.Find("Generated Chitose Candy");
+
+				Assert.That(liveRoot.PublicParameterIds, Is.EquivalentTo(new[] {
+					ChitoseCandyCutScene.SplitGapParameterId,
+					ChitoseCandyCutScene.HorizontalImpulseParameterId
+				}));
+				Assert.That(liveRoot.TrySetParameter(
+					ChitoseCandyCutScene.SplitGapParameterId, 1.25f, out var splitGapRejection), Is.True);
+				Assert.That(splitGapRejection, Is.Empty);
+				Assert.That(liveRoot.TrySetParameter(
+					ChitoseCandyCutScene.HorizontalImpulseParameterId, 4.5f, out var impulseRejection), Is.True);
+				Assert.That(impulseRejection, Is.Empty);
+				Assert.That(cutScene.SplitGap, Is.EqualTo(1.25f));
+				Assert.That(cutScene.HorizontalImpulse, Is.EqualTo(4.5f));
 				Assert.That(root.transform.GetChild(root.transform.childCount - 1), Is.SameAs(generatedRoot));
 			}
 			finally {
