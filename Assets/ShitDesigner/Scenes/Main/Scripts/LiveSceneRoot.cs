a9cab66e7a7fcda271d3a491c741335a44a5b942
POTI <potistudio@proton.me>
2026-09-01 18:05:26 +0900

    Trigger AssetFlush from unquantized hot cues

--- Assets/ShitDesigner/Scenes/Main/Scripts/LiveSceneRoot.cs
+++ Assets/ShitDesigner/Scenes/Main/Scripts/LiveSceneRoot.cs
@@ -14,9 +14,8 @@ namespace ShitDesigner.Main {
 		public float Minimum { get; }
 		public float Maximum { get; }
 		public float Value { get; }
-		public bool IsTrigger { get; }
 
-		public LiveParameterDefinition(string id, string displayName, float minimum, float maximum, float value, bool isTrigger = false) {
+		public LiveParameterDefinition(string id, string displayName, float minimum, float maximum, float value) {
 			Id = id;
 			DisplayName = displayName;
 			Type = ParameterType.Float;
@@ -25,10 +24,9 @@ namespace ShitDesigner.Main {
 			Minimum = minimum;
 			Maximum = maximum;
 			Value = value;
-			IsTrigger = isTrigger;
 		}
 
-		public LiveParameterDefinition(string id, string displayName, ParameterValue value, bool isTrigger = false) {
+		public LiveParameterDefinition(string id, string displayName, ParameterValue value) {
 			Id = id;
 			DisplayName = displayName;
 			Type = value.Type;
@@ -37,13 +35,7 @@ namespace ShitDesigner.Main {
 			Minimum = 0f;
 			Maximum = 0f;
 			Value = value.Type == ParameterType.Float ? value.AsFloat() : 0f;
-			IsTrigger = isTrigger;
 		}
-
-		internal LiveParameterDefinition WithTrigger(bool isTrigger)
-			=> HasRange
-				? new LiveParameterDefinition(Id, DisplayName, Minimum, Maximum, Value, isTrigger)
-				: new LiveParameterDefinition(Id, DisplayName, TypedValue, isTrigger);
 	}
 
 	/// <summary>
@@ -75,9 +67,7 @@ namespace ShitDesigner.Main {
 
 		public LiveParameterDefinition[] GetParameterDefinitions() {
 			CollectParameters();
-			return _parameters.Values
-				.Select(parameter => parameter.Definition.WithTrigger(parameter is ILiveSceneTriggerParameter))
-				.ToArray();
+			return _parameters.Values.Select(parameter => parameter.Definition).ToArray();
 		}
 
 		public bool TrySetParameter(string parameterId, float value, out string rejectionReason) {
