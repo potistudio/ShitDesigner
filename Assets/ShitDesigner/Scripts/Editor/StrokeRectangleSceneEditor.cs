d11003834bbdd7940fbcfb2b7d1dc49a9b0a07e4
POTI <potistudio@proton.me>
2026-09-02 15:23:22 +0900

    Add repeated cuboid trigger bursts

--- Assets/ShitDesigner/Scripts/Editor/StrokeRectangleSceneEditor.cs
+++ Assets/ShitDesigner/Scripts/Editor/StrokeRectangleSceneEditor.cs
@@ -10,7 +10,7 @@ namespace ShitDesigner.Editor {
 			EditorGUILayout.Space();
 
 			using (new EditorGUI.DisabledScope(!UnityEngine.Application.isPlaying))
-				if (GUILayout.Button("Trigger Up / Fade / Depth"))
+				if (GUILayout.Button("Trigger Burst"))
 					TriggerSelectedCuboids();
 
 			if (!UnityEngine.Application.isPlaying)
